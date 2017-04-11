0ded37a766177b9df93cfafc31a49893f7f01fdd
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-11 17:30:23 +0200

    Unhandled exception should result in 500er response

--- src/lib.js
+++ src/lib.js
@@ -3,6 +3,14 @@
 module.exports = function add(app, controllers) {
   for (const controller of controllers) {
     if (typeof controller[0] === 'string') {
+      const handle = controller[1][controller[1].length - 1];
+      controller[1][controller[1].length - 1] = async (req, res) => {
+        try {
+          await handle(req, res);
+        } catch (e) {
+          res.sendStatus(e.status || 500);
+        }
+      };
       app[controller[0]](...controller[1]);
     } else {
       add(app, controller);
