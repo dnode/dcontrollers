44b268caedc5b9531f72d4703b5b138881ebdb23
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-12 12:17:15 +0200

    Extract the handler logic and also wrap middlewares

--- src/lib.js
+++ src/lib.js
@@ -1,16 +1,19 @@
 'use strict';
 
+const dhandler = require('./dhandler');
+
 module.exports = function add(app, controllers) {
   for (const controller of controllers) {
     if (typeof controller[0] === 'string') {
-      const handle = controller[1][controller[1].length - 1];
-      controller[1][controller[1].length - 1] = async (req, res) => {
-        try {
-          await handle(req, res);
-        } catch (e) {
-          res.sendStatus(e.status || 500);
+      controller[1] = controller[1].map((value, index) => {
+        if (index === 0) {
+          return value;
         }
-      };
+        if (Array.isArray(value)) {
+          return value.map(middleware => dhandler(middleware));
+        }
+        return dhandler(value);
+      });
       app[controller[0]](...controller[1]);
     } else {
       add(app, controller);
