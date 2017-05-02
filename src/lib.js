1bfecd686079969607b26d66c07624be76a35575
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-02 15:57:10 +0200

    Allow controllers to be a single controller

--- src/lib.js
+++ src/lib.js
@@ -3,6 +3,9 @@
 const dhandler = require('dhandler');
 
 module.exports = function add(app, controllers) {
+  if (!Array.isArray(controllers)) {
+    controllers = [controllers];
+  }
   for (const controller of controllers) {
     if (typeof controller[0] === 'string') {
       controller[1] = controller[1].map((value, index) => {
